eb054f74dbb2a3ab412d1f5db3b2fed98147ab7f
Arup Kumar Gupta <arup.shibai@gmail.com>
2019-08-16 22:12:58 +0530

    initial commit and create env scripts

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const routes = require('./routes/routes');
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api',routes);
+
+app.listen(process.env.PORT,() => {
+    console.log(`Listening on ${process.env.PORT} environment: ${process.env.NODE_ENV}`);
+})
